16bedae1168af8e927515c16803fc8c56a683514
mrbrightsiide <devkimsia@gmail.com>
2022-01-07 23:33:02 +0900

    1. Add videoPlayer.js to webpack entry

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,16 +1,35 @@
 import express from "express";
-import { getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin, getChnagePassword, postChnagePassword} from "../controllers/userControllers.js";
-import { protectorMiddleware, publicOnlyMiddleware, avatarUpload } from "../middlewares.js";
- 
+import {
+  getEdit,
+  postEdit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+  getChnagePassword,
+  postChnagePassword,
+} from "../controllers/userControllers.js";
+import {
+  protectorMiddleware,
+  publicOnlyMiddleware,
+  avatarUpload,
+} from "../middlewares.js";
+
 const userRouter = express.Router();
 
-
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
-userRouter.route("/change-password").all(protectorMiddleware).get(getChnagePassword).post(postChnagePassword);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChnagePassword)
+  .post(postChnagePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
-
 
 export default userRouter;
--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,39 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: "./src/client/js/main.js",
-    mode: "development",
-    watch: true,
-    output:{
-        filename: "js/main.js",
-        path: path.resolve(__dirname, "assets"),
-        clean:true
-        // 새로운 output폴더를 빌드하면, 기존에 있던 사용하지 않게되는 output폴더를 지워주는 속성.
-    },
-    plugins: [new MiniCssExtractPlugin({
-        filename:"css/styles.css"
-    })],
-    // 그냥코드문법임. 문서만 따라가도 됨
-    module: {
-        rules: [
-            {
-                test:/\.js$/,
-                use:{
-                    loader: "babel-loader",
-                    options: {
-                        presets: [['@babel/preset-env', { targets: "defaults" }]]
-                    },
-                },
-            },
-        {
-            test: /\.scss$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+  entry: {
+    main: "./src/client/js/main.js",
+    videoplayer: "./src/client/js/videoPlayer.js",
+  },
+  mode: "development",
+  watch: true,
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
+    // 새로운 output폴더를 빌드하면, 기존에 있던 사용하지 않게되는 output폴더를 지워주는 속성.
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  // 그냥코드문법임. 문서만 따라가도 됨
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
         },
-        ],
-    },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
