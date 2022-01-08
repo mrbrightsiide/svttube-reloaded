a89c3746d4dc186719fb1cf568f04b092909c13c
mrbrightsiide <devkimsia@gmail.com>
2022-01-08 17:29:06 +0900

    Add currentTime and totlaTime to watch video page

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,9 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
-
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("toatalTime");
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -42,6 +42,16 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const handleLoadedMetaData = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
