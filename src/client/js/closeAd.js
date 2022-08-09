eeaae5c3a20c74599df680a40732b06a26679a74
SiaKim <devkimsia@gmail.com>
2022-08-09 21:24:22 +0900

    feat: Add masthead-ad to home.pug

--- src/client/js/closeAd.js
+++ src/client/js/closeAd.js
@@ -2,7 +2,7 @@ const handleAdClose = (event) => {
   if (event.target.id === "close-btn") {
     event.preventDefault();
     const mainAd = event.target.parentElement;
-    mainAd.className = "close";
+    mainAd.remove();
   }
 };
 
