0a23f46fcdac1cb08ce7a9759a04cde8f176afde
SiaKim <devkimsia@gmail.com>
2022-08-19 21:47:37 +0900

    chore: Remove createTumbail from apiRouter.js and videoControllers.js

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -4,13 +4,11 @@ import {
   createComment,
   deleteComment,
   getVideos,
-  createTumbnail,
 } from "../controllers/videoControllers.js";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/category/:id", getVideos);
-apiRouter.post("/thumbnail", createTumbnail);
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
 apiRouter.delete("/comment/:id([0-9a-f]{24})/delete", deleteComment);
