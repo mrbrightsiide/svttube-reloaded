32c3ae4f420b8f724604fb91dc73913aefaa5142
SiaKim <devkimsia@gmail.com>
2022-08-19 21:53:08 +0900

    feat: Create video thumbnail using ffmpeg in getThumbnails.js

--- src/client/js/getThumbnails.js
+++ src/client/js/getThumbnails.js
@@ -1,22 +1,67 @@
-const inputVideo = document.getElementById("input");
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const handleFileChanged = async (e) => {
+const formBox = document.querySelector(".form-box");
+
+const inputVideo = document.querySelector("#videoInput");
+
+const onUploadVideo = async (e) => {
   e.preventDefault();
-  if (e.currentTarget.files.length >= 1) {
-    let data = new FormData();
-    data.append("file", e.currentTarget.files[0]);
-    // for (var pair of formData.entries()) {
-    //   console.log(pair[0] + ", " + pair[1]);
-    // }
-    // 비디오파일 url을 ffmpeg로 보내기
-    const response = await fetch(`/api/thumbnail`, {
-      method: "POST",
-      body: e.currentTarget.files[0],
-    });
-    const res = await response.json();
-    console.log(res);
-    // console.log("썸네일 그리기");
-  }
+  let videoFile = window.URL.createObjectURL(inputVideo.files[0]);
+
+  const files = {
+    input: "recording.webm",
+    output: "output.mp4",
+    thumb: "thumbnail.jpg",
+  };
+
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+
+  // await ffmpeg.run("-i", files.input, "-r", "60", files.output);
+  await ffmpeg.run(
+    "-i",
+    files.input,
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    files.thumb
+  );
+
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+  const thumbUrl = URL.createObjectURL(thumbBlob);
+  console.log(thumbUrl);
+  // downloadFile(thumbUrl, "MyThumbnail.jpg");
+  // ffmpeg.FS("unlink", files.input);
+  // ffmpeg.FS("unlink", files.output);
+  // ffmpeg.FS("unlink", files.thumb);
+
+  const thumbPreview = document.createElement("img");
+
+  thumbPreview.setAttribute("src", thumbUrl);
+
+  formBox.append(thumbPreview);
+
+  // URL.revokeObjectURL(thumbUrl);
+
+  // console.log(fileUrl);
+
+  // if (fileUrl) {
+  //   // 비디오파일 url을 ffmpeg로 보내기
+  //   await fetch(`/api/thumbnail`, {
+  //     method: "POST",
+  //     headers: {
+  //       videofile: fileUrl,
+  //     },
+  //   })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // }
+  // const hmmm = await response.json();
+  // dom으로 썸네일 그리기;
 };
 
-inputVideo.addEventListener("change", handleFileChanged);
+inputVideo.addEventListener("change", onUploadVideo);
