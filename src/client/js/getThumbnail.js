66558c8a8b05ee0bda1a5bd0fdfbbfdec1c97500
SiaKim <devkimsia@gmail.com>
2022-08-25 20:25:24 +0900

    style: Add style for thumbnails preview and selecting them

--- src/client/js/getThumbnail.js
+++ src/client/js/getThumbnail.js
@@ -1,8 +1,12 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const formBox = document.querySelector(".thumbnailpreview");
+const formBox = document.querySelector(".thumbnail-preview");
 const inputThumb = document.querySelector("#thumb");
+const thumbUploadBtn = document.querySelector(".upload-thumb-btn");
+const inputThumbPreview = document.querySelector(".thumbnail-input-preview");
 const inputVideo = document.querySelector("#videoInput");
+let createdThumbfileUrl = "";
+let uploadedThumbfile = "";
 
 const onUploadVideo = async (e) => {
   e.preventDefault();
@@ -30,26 +34,61 @@ const onUploadVideo = async (e) => {
   );
 
   const thumbFile = ffmpeg.FS("readFile", files.thumb);
+  createdThumbfileUrl = thumbFile;
   const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const thumbPreview = document.createElement("img");
-
-  thumbPreview.setAttribute("src", thumbUrl);
-  thumbPreview.setAttribute("id", "thumb-preview");
-  thumbPreview.setAttribute("style", "object-fit:cover");
+  if (formBox.childNodes[0]) {
+    formBox.childNodes[0].remove();
+  }
+  formBox.setAttribute("style", `background-image:url(${thumbUrl})`);
   formBox.classList.add("selected");
-
-  formBox.append(thumbPreview);
-
+  inputThumbPreview.classList.remove("selected");
   const myFile = new File([thumbFile], "thumbnail.jpg", {
     type: "image/jpeg",
   });
-
-  // Create a DataTransfer to get a FileList
   const dataTransfer = new DataTransfer();
   dataTransfer.items.add(myFile);
   inputThumb.files = dataTransfer.files;
 };
 
+const onUploadThumbnail = (e) => {
+  uploadedThumbfile = e.target.files;
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    if (inputThumbPreview.childNodes[0]) {
+      inputThumbPreview.childNodes[0].remove();
+    }
+    inputThumbPreview.setAttribute(
+      "style",
+      `background-image:url(${e.target.result})`
+    );
+    inputThumbPreview.classList.add("selected");
+    formBox.classList.remove("selected");
+  };
+  reader.readAsDataURL(inputThumb.files[0]);
+};
+
 inputVideo.addEventListener("change", onUploadVideo);
+inputThumb.addEventListener("change", onUploadThumbnail);
+thumbUploadBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputThumb.click();
+});
+formBox.addEventListener("click", (e) => {
+  formBox.classList.add("selected");
+  inputThumbPreview.classList.remove("selected");
+  const myFile = new File([createdThumbfileUrl], "thumbnail.jpg", {
+    type: "image/jpeg",
+  });
+  console.log(createdThumbfileUrl);
+  const dataTransfer = new DataTransfer();
+  dataTransfer.items.add(myFile);
+  inputThumb.files = dataTransfer.files;
+});
+
+inputThumbPreview.addEventListener("click", (e) => {
+  inputThumb.files = uploadedThumbfile;
+  inputThumbPreview.classList.add("selected");
+  formBox.classList.remove("selected");
+});
