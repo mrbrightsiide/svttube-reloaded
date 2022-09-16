879357a5738b0d15e5cccff3df49383de214bafe
SiaKim <devkimsia@gmail.com>
2022-09-16 17:31:43 +0900

    feat: Add nav bar style to video page

--- src/client/js/handleNavBar.js
+++ src/client/js/handleNavBar.js
@@ -1,101 +1,103 @@
 const mediaQ = window.matchMedia("screen and (min-width: 1326px)");
 const mediaQMiddle = window.matchMedia(
-  "(max-width: 1325px) and (min-width: 680px)"
+  "(max-width: 1325px) and (min-width: 720px)"
 );
 const mediaQMin = window.matchMedia(
-  "(max-width: 679px) and (min-width: 300px)"
+  "(max-width: 719px) and (min-width: 300px)"
 );
-const navBtn = document.querySelector("#guide_menu_btn");
+const navBtn = document.querySelectorAll("#guide_menu_btn");
 const navContainer = document.querySelector(".guide-container");
 const page = document.querySelector(".page-manager");
-const modalWrap = document.createElement("div");
-const modal = document.createElement("div");
+const videoPage = window.location.pathname.includes("videos");
 
-modalWrap.classList.add("modal-wrap");
-modalWrap.append(modal);
-document.body.prepend(modalWrap);
+const createModal = () => {
+  const modalWrap = document.createElement("div");
+  const modal = document.createElement("div");
+  modalWrap.classList.add("modal-wrap");
+  modalWrap.append(modal);
+  document.body.prepend(modalWrap);
+  modalWrap.classList.add("show");
 
-if (window.location.pathname.includes("videos")) {
-  navContainer.classList.add("close");
-  page.classList.add("close");
-}
+  modalWrap.addEventListener("click", (event) => {
+    deleteModal();
+    if (event.target === modalWrap) {
+      modalWrap.classList.toggle("show");
+      navContainer.classList.toggle("modal");
+
+      if (mediaQMiddle.matches && !videoPage) {
+        navContainer.classList.toggle("mini");
+      }
+
+      if (mediaQMin.matches || videoPage) {
+        navContainer.classList.toggle("close");
+      }
+    }
+  });
+};
+
+const deleteModal = () => {
+  const modal = document.body.querySelector(".modal-wrap");
+  if (modal) {
+    modal.remove();
+  }
+};
 
 const handleNavBtn = (e) => {
-  // mini toggle
-  if (mediaQ.matches) {
-    navBtn.classList.remove("close");
+  if (!mediaQ.matches && !videoPage) {
+    navContainer.classList.contains("modal") ? deleteModal() : createModal();
+  }
+
+  if (videoPage) {
+    navContainer.classList.contains("modal") ? deleteModal() : createModal();
+  }
+
+  if (mediaQ.matches && !videoPage) {
     navContainer.classList.toggle("mini");
     page.classList.toggle("mini");
   }
 
-  // mini as default.
-  if (mediaQMiddle.matches) {
-    navContainer.classList.toggle("mini");
+  if (mediaQMiddle.matches && !videoPage) {
     navContainer.classList.toggle("modal");
-    modalWrap.classList.toggle("show");
-    if (modalWrap.classList.contains("show")) {
-      document.body.style.overflow = "hidden";
-    }
+    navContainer.classList.toggle("mini");
   }
 
-  // close as default
-  if (mediaQMin.matches || window.location.pathname.includes("videos")) {
-    navContainer.classList.toggle("close");
+  if (mediaQMin.matches || videoPage) {
     navContainer.classList.toggle("modal");
-    modalWrap.classList.toggle("show");
-    if (modalWrap.classList.contains("show")) {
-      document.body.style.overflow = "hidden";
-    }
+    navContainer.classList.toggle("close");
   }
 };
-
-modalWrap.addEventListener("click", (event) => {
-  if (event.target === modalWrap) {
-    modalWrap.classList.toggle("show");
-
-    if (mediaQMiddle.matches) {
-      navContainer.classList.toggle("modal");
-      navContainer.classList.toggle("mini");
-    }
-
-    if (mediaQMin.matches || window.location.pathname.includes("videos")) {
-      navContainer.classList.toggle("modal");
-      navContainer.classList.toggle("close");
-    }
-
-    if (!modalWrap.classList.contains("show")) {
-      document.body.style.overflow = "auto";
-    }
-  }
-});
-
-navBtn.addEventListener("click", handleNavBtn);
 
 const handleMeadia = (e) => {
-  if (!window.location.pathname.includes("videos")) {
-    if (mediaQ.matches) {
-      navContainer.classList.remove("close");
-      page.classList.remove("close");
-      navContainer.classList.remove("mini");
-      page.classList.remove("mini");
-    }
+  if (navContainer.classList.contains("modal")) {
+    const modalWrap = document.querySelector(".modal-wrap");
+    modalWrap.remove();
+    navContainer.classList.remove("modal");
+  }
 
-    if (mediaQMiddle.matches) {
-      navContainer.classList.remove("close");
-      page.classList.remove("close");
-      navContainer.classList.add("mini");
-      page.classList.add("mini");
-    }
+  if (page.classList.contains("close")) {
+    navContainer.classList.remove("close");
+    page.classList.remove("close");
+  }
 
-    if (mediaQMin.matches) {
-      navContainer.classList.remove("mini");
-      page.classList.remove("mini");
-      navContainer.classList.add("close");
-      page.classList.add("close");
-    }
+  if (page.classList.contains("mini")) {
+    navContainer.classList.remove("mini");
+    page.classList.remove("mini");
+  }
+
+  if (mediaQMiddle.matches && !videoPage) {
+    navContainer.classList.add("mini");
+    page.classList.add("mini");
+  }
+
+  if (mediaQMin.matches || videoPage) {
+    navContainer.classList.add("close");
+    page.classList.add("close");
   }
 };
 
-handleMeadia();
 mediaQ.addEventListener("change", handleMeadia);
 mediaQMin.addEventListener("change", handleMeadia);
+navBtn.forEach((btn) => {
+  btn.addEventListener("click", handleNavBtn);
+});
+handleMeadia();
