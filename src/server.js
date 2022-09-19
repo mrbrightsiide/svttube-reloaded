07ec9534eb362e2c70c316faebf9bf686a9329ce
SiaKim <devkimsia@gmail.com>
2022-09-19 16:47:26 +0900

    chore: Change mongoUrl in session

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/svttube" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
