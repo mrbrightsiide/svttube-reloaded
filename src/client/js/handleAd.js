61c2f03843888a927aa0e418aedd79c126122dca
SiaKim <devkimsia@gmail.com>
2022-09-27 20:38:25 +0900

    chore: Update ad videos link

--- src/client/js/handleAd.js
+++ src/client/js/handleAd.js
@@ -2,20 +2,20 @@
 const adList = [
   {
     text: "_WORLD",
-    videoId: "cda5d66617537d4c741a9653f95ef2ac",
-    videoLink: "62f2474ecf12933da830ef99",
+    videoId: "fb0e4fed4fd8d458d09911666581ea4d",
+    videoLink: "632ed232842cde0016a0aa3c",
     bgColor: "#f1ebff",
   },
   {
     text: "Rock with you",
-    videoId: "88f2d2e2971853e27ef8261e8c7bc4c2",
-    videoLink: "6322f49a9f87a74618ff25bc",
+    videoId: "033d252fe12e1e8964201dd359d46ecb",
+    videoLink: "632ed232842cde0016a0aa3c",
     bgColor: "#dbf0ff",
   },
   {
     text: "HOT",
-    videoId: "37e8424771ac397df014c385136a9d7a",
-    videoLink: "6327befe8894e336a8285d9b",
+    videoId: "033d252fe12e1e8964201dd359d46ecb",
+    videoLink: "6332dedcf891580016cfbd9b",
     bgColor: "#ffe6d1",
   },
 ];
@@ -28,8 +28,14 @@ const adBtnAnchor = document.querySelector("#banner-btn");
 const videoContainer = document.querySelector("#videoContainer");
 
 adText.innerHTML = `Check out the new song<br>"${currentAd.text}" by Seventeen!`;
-adVideo.setAttribute("src", `/uploads/videos/${currentAd.videoId}`);
-adBtnAnchor.setAttribute("href", `/videos/${currentAd.videoLink}`);
+adVideo.setAttribute(
+  "src",
+  `https://svttube.s3.amazonaws.com/videos/${currentAd.videoId}`
+);
+adBtnAnchor.setAttribute(
+  "href",
+  `https://svttube.herokuapp.com/videos/${currentAd.videoLink}`
+);
 adContainer.setAttribute("style", `background:${currentAd.bgColor}`);
 
 //Close ad when close button clicked
